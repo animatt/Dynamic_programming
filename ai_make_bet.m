cab18deb3a8e53181119a3a25791ffe2972cd851
King Crawford <crawboy@me.com>
2017-01-25 19:03:30 -0500

    added comment to ai_make_bet

--- ai_make_bet.m
+++ ai_make_bet.m
@@ -1,5 +1,6 @@
 function [bank, stake] = ai_make_bet(bank, goal, probability_of_heads)
-% logic to determine stake
-
+% value iteration
+% v_k+1 = max_a(q?(s, a)) 
+%   = max_a(E[R_t+1 + y * v_k(S_t+1)|S_t = s, A_t = a])
 bank = bank - stake;
 end
